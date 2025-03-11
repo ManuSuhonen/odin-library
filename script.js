7ac7e711b5eec82295fcfbb1f50a8684d33c8873
Manu Suhonen <manu.suhonen@gmail.com>
2025-03-11 23:55:52 +0200

    WIP 1

---
+++ script.js
@@ -0,0 +1,13 @@
+const dialog = document.querySelector("dialog");
+const showButton = document.querySelector("header > button");
+const closeButton = document.querySelector("dialog button");
+
+// "Show the dialog" button opens the dialog modally
+// showButton.addEventListener("click", () => { dialog.showModal(); });
+
+// "Close" button closes the dialog
+// closeButton.addEventListener("click", () => { dialog.close(); });
+
+// btn = document.querySelector(".btn.btn-primary.last");
+
+// btn.onclick = function () {alert("asdsd");}
